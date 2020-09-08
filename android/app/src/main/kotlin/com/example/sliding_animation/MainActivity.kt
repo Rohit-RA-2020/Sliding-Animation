ddc8503f4783c4ad1ccef610cfe760bb0243998c
Rohit ranjan <ranjanrohit812@gmail.com>
2020-09-08 22:11:21 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/sliding_animation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sliding_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MultipleDelayedSlideAnimations());
+
+class MultipleDelayedSlideAnimations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        color: Colors.white,
+        child: SafeArea(child: Scaffold(body: buildBody())),
+      ),
+    );
+  }
+
+  Widget buildBody() {
+    return Container(child: MultipleSlidingBox());
+  }
+}
+
+class MultipleSlidingBox extends StatefulWidget {
+  @override
+  _MultipleSlidingBoxState createState() => _MultipleSlidingBoxState();
+}
+
+class _MultipleSlidingBoxState extends State<MultipleSlidingBox>
+    with SingleTickerProviderStateMixin {
+  Animation slidingAnimation;
+  Animation delayedSlidingAnimation;
+  Animation furtherDelayedSlidingAnimation;
+  AnimationController slidingBoxAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    slidingBoxAnimationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+
+    slidingAnimation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+        parent: slidingBoxAnimationController,
+        curve: Interval(0.0, 1.0, curve: Curves.fastOutSlowIn)));
+
+    delayedSlidingAnimation = Tween(begin: -1.0, end: 0.0).animate(
+        CurvedAnimation(
+            parent: slidingBoxAnimationController,
+            curve: Interval(0.23, 1.0, curve: Curves.fastOutSlowIn)));
+
+    furtherDelayedSlidingAnimation = Tween(begin: -1.0, end: 0.0).animate(
+        CurvedAnimation(
+            parent: slidingBoxAnimationController,
+            curve: Interval(0.43, 1, curve: Curves.fastOutSlowIn)));
+
+    slidingBoxAnimationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+
+    return AnimatedBuilder(
+      animation: slidingBoxAnimationController,
+      builder: (BuildContext context, Widget child) {
+        return Column(
+          children: <Widget>[
+            Transform(
+              transform: Matrix4.translationValues(
+                  slidingAnimation.value * width, 0.0, 0.0),
+              child: Center(
+                child: Container(height: 200, width: 200, color: Colors.red),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Transform(
+              transform: Matrix4.translationValues(
+                  delayedSlidingAnimation.value * width, 0.0, 0.0),
+              child: Center(
+                child: Container(height: 200, width: 200, color: Colors.blue),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Transform(
+              transform: Matrix4.translationValues(
+                  furtherDelayedSlidingAnimation.value * width, 0.0, 0.0),
+              child: Center(
+                child: Container(
+                    height: 200, width: 200, color: Colors.lightGreenAccent),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
